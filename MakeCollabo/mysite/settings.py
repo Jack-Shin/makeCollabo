c93a124c04e29f236b3b20dee37ef3c60ebfe5c6
Chris-Chang <hw@yunbin.kr>
2018-01-06 00:21:06 +0900

    change base.html

--- MakeCollabo/mysite/settings.py
+++ MakeCollabo/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o=b5+q@f9#&ylnz(e=q_q^v^=+(y-0o^z5dq&dni=o#%_y!$dp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
